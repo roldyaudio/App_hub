ff5c84a814d3a84804c4b091b068e7466e030872
roldy <roldyaudio@gmail.com>
2025-07-05 20:32:38 +0200

    Modified installer function to point to App_Hub and Apps

--- gui.py
+++ gui.py
@@ -102,6 +102,7 @@ ctk.set_window_scaling(True)
 
 ensure_pip()
 install_requirements_in_directory("C:/00_repos/App_hub")
+install_requirements_in_directory("C:/Apps")
 app = App()
 center_hub(app, 350, 350)
 app.mainloop()
