d494519bd7baf29a5796240143f9f4c761f1db59
rrobles <roldyaudio@gmail.com>
2025-07-10 17:44:41 +0200

    Some minor changes

--- repo_manager.py
+++ repo_manager.py
@@ -2,6 +2,7 @@ import subprocess
 import json
 import os
 import threading
+import sys
 
 # JSON FUNCTIONS
 def load_repos():
@@ -25,7 +26,7 @@ def clone_or_update_repo(repo_url, download_path, file_to_run):
             result = subprocess.run(["git", "-C", repo_path, "pull"], capture_output=True, text=True)
             if result.returncode == 0:
                 print(f"Repository {repo_url} updated successfully.")
-                restart_program()
+                # restart_program()
             else:
                 print(f"Error updating repository: {result.stderr}")
             # run_file(repo_path, file_to_run)  # Run the file after updating
