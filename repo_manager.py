e69f3be323fb6def6500965b6d3f2c363bcccb2d
roldyaudio <roldyaudio@gmail.com>
2025-07-10 17:06:09 +0200

    Update repo_manager.py

--- repo_manager.py
+++ repo_manager.py
@@ -25,9 +25,10 @@ def clone_or_update_repo(repo_url, download_path, file_to_run):
             result = subprocess.run(["git", "-C", repo_path, "pull"], capture_output=True, text=True)
             if result.returncode == 0:
                 print(f"Repository {repo_url} updated successfully.")
+                restart_program()
             else:
                 print(f"Error updating repository: {result.stderr}")
-            run_file(repo_path, file_to_run)  # Run the file after updating
+            # run_file(repo_path, file_to_run)  # Run the file after updating
         else:
             # Clone the repository if it doesn't exist
             result = subprocess.run(["git", "clone", repo_url, repo_path], capture_output=True, text=True)
