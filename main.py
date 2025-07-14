1e472985aa0e9ac73d068455cbf251455307b0d1
roldyaudio <roldyaudio@gmail.com>
2025-07-14 10:48:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ updated = install_requirements_in_directory("C:/Apps/App_hub")
 
 if updated:
     print("\n\x1b[0;36mRestarting the program to include updates...\x1b[0m")
+    print("test")
     os.execv(sys.executable, [sys.executable] + sys.argv)
 else:
     print("\nNo updates were made, continuing execution...")
